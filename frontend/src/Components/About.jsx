e1ae91fe7b2a66d76aea57a22b34ba6ad27dee7f
safwan ahmad <safwan8ahmad9@gmail.com>
2026-08-28 13:00:28 +0530

    cahnge avbout

--- frontend/src/Components/About.jsx
+++ frontend/src/Components/About.jsx
@@ -2,12 +2,10 @@ import { useContext, useEffect } from 'react'
 import NotesContext from '../Context/NotesContext'
 export default function About() {
   const notes = useContext(NotesContext)
-  useEffect(() => {
-    notes.update()
-  }, [])
+  
   return (
     <div>
-      <h1>About {notes.state.name} and {notes.state.class}</h1>
+      <h1>About Notes</h1>
     </div>
   )
 }
